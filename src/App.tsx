0797f36cd3da68e303ea132b8521f34a1c4a8394
jcagz96 <jcagz@iscte-iul.pt>
2022-12-22 20:02:30 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import './styles/global.css';
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h2 className="font-bold">hello joca</h2>
+  )
+}
