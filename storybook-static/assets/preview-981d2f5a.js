1663ad2f648bb2e47d8492daccc4ff79e66250b0
jcagz96 <jcagz@iscte-iul.pt>
2022-12-22 23:43:59 +0000

    Gitignore issue fixed

--- storybook-static/assets/preview-981d2f5a.js
+++
@@ -1,2 +0,0 @@
-import{t as o}from"./index-d4b20078.js";import"./es.object.get-own-property-descriptor-b0ef2764.js";import"./es.map.constructor-1d883da4.js";import"./es.number.to-fixed-a322f1b1.js";import"./index-cb59b577.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
-//# sourceMappingURL=preview-981d2f5a.js.map
